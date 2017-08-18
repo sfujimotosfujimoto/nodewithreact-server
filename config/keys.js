5320cba25d5c71b4f3c1b9ed8a8efa4db662845c
S Fujimoto <unicoshun@gmail.com>
2017-08-18 22:26:27 +0900

    3

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js
+
+if (process.env.NODE_ENV === 'production') {
+  // we are in prod - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+ // we are in dev - return the dev keys!
+  module.exports = require('./dev');
+}
